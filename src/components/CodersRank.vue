f395ecd49a6177ad3b4f6783e9f6d31c31a5c318
Hermesis <hermesiss@gmail.com>
2020-02-13 02:28:26 +0400

    Add CodersRank component

---
+++ src/components/CodersRank.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="codersrank" :username="username"></div>
+</template>
+
+<script>
+const setAttr = (container, widget, attr) => {
+  widget.setAttribute(attr, container.getAttribute(attr));
+};
+
+export default {
+  data: () => ({}),
+  mounted() {
+    let scriptElement = document.createElement("script");
+    scriptElement.setAttribute(
+      "src",
+      "https://profile.codersrank.io/widget/widget.js"
+    );
+    scriptElement.async = true;
+    document.head.appendChild(scriptElement);
+    let widgetContainer = document.getElementById("codersrank");
+    let widget = document.createElement("codersrank-widget");
+    setAttr(widgetContainer, widget, "username");
+    //widget.setAttribute('username', widgetContainer.getAttribute('username'))
+    widgetContainer.appendChild(widget);
+  },
+  props: {
+    username: String
+  }
+};
+</script>
+<style>
+codersrank-widget {
+  --head-bg-color: var(--color-base-1);
+  --bg-color: var(--color-base);
+  --badge-bg-color: var(--color-base-1);
+  --badge-text-color: var(--color-contrast);
+}
+</style>
+
+// <CodersRank username="hermesiss" />
+// import CodersRank from "./CodersRank.vue";
+
+//   components: {
+//     CodersRank
+//   }
