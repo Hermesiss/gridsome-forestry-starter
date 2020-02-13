73228645414e37870c2b49951c40fa709e072638
Hermesis <hermesiss@gmail.com>
2020-02-13 10:52:14 +0400

    Add chart component [skip ci]

---
+++ src/pages/Doughnut.vue
@@ -0,0 +1,26 @@
+/// https://vue-chartjs.org/
+<script>
+import { Doughnut } from "vue-chartjs";
+export default {
+  extends: Doughnut,
+  data: () => ({
+    chartdata: {
+      labels: ["1", "2", "3"],
+      datasets: [
+        {
+          label: "Data One",
+          backgroundColor: "#999",
+          data: [40, 20, 10, 5]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+};
+</script>
